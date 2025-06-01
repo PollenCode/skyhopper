c1620ccb9bf3af384e87cbea68762a980a159f6b
Stijn Rogiest <reddusted@gmail.com>
2025-06-01 13:35:24 +0200

    First working socket

--- index.py
+++ index.py
@@ -1,16 +1,20 @@
+import os
+import sys
 import time
-import cv2
+# import cv2
 import threading
 import crypt
 import hashlib
 import struct
-
-
+import socket
 
 # image = cv2.imread("image.jpg")
 
 # cv2.imshow('image',image)
 # cv2.waitKey(0)
+
+LOCALHOST = "127.0.0.1"
+PORTS = [3456, 7689, 10023, 33332]
 
 
 class SkyHopperDevice:
@@ -31,6 +35,9 @@ class SkyHopperDevice:
         self.current_freq = 0
         self.interval = 1
 
+
+
+
     def get_freq_idx(self):
         frame_idx = self.get_time() // self.interval
         hashed = hashlib.sha1(self.key + bytearray(struct.pack("f", frame_idx)))
@@ -46,7 +53,6 @@ class SkyHopperDevice:
         freq_idx = self.get_freq_idx()
         self.current_freq = freq_idx
         freq =  ["WiFi", "Bt", "LoRa"][freq_idx]
-        
 
         print(int(self.get_time()), ": Channel updated:", freq)
         # TODO: update the receiver/transmitter frequency
@@ -67,18 +73,39 @@ class SkyHopperDevice:
         pass
 
 
-inst = SkyHopperDevice(bytes([1,2,3]))
-inst.sync_time()
+# inst = SkyHopperDevice(bytes([1,2,3]))
+# inst.sync_time()
 
 
-while True:
-    # print("time", inst.get_time(), "-> freq", inst.get_freq_idx())
-    inst._update_freq()
-    time.sleep(1)
-
-
-
+# while True:
+#     # print("time", inst.get_time(), "-> freq", inst.get_freq_idx())
+#     inst._update_freq()
+#     time.sleep(1)
 
 
 
 
+if sys.argv[1] == "recv":
+    print("receiver starting")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('',8888))
+    sock.setblocking(False)
+
+    while True:
+        try:
+            data, address = sock.recvfrom(10000)
+            print("received:", bytes(data).decode())
+        except socket.error as ex:
+            # print("Receive error: ", type(ex), ex)
+            pass
+        # else: 
+        #     print("recv:", data[0],"times",len(data))
+        time.sleep(1)
+else:
+    print("sending")
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+    while True:
+        sock.sendto("hallo".encode(), ("127.0.0.1", 8888))
+        time.sleep(1.5)
